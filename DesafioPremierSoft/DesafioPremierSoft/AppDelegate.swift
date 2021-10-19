e94cf0c6e84546ffa229a1ef8bb3b9262f0d67f7
Leonardo Cunha <cunhacardosoleo@gmail.com>
2021-10-19 17:59:05 -0300

    Criando projeto

---
+++ DesafioPremierSoft/DesafioPremierSoft/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  DesafioPremierSoft
+//
+//  Created by Leonardo Cunha on 19/10/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        
+        return true
+    }
+
+}
+
